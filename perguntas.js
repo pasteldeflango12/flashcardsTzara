5373cd89057a53ee5475c098d03391ea3c2c9b98
Tzara1955 <tzarafujii@gmail.com>
2024-11-01 14:25:34 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,52 @@
+criaCartao(
+    'Infância',
+    'Onde é a casa de infância da cantora?',
+    'Highland Park de Los Angeles, na Califórnia.'
+)
+
+criaCartao(
+    'Pré adolescência',
+    'Com quantos anos Billie lançou sua primeira canção junto a seu irmão?',
+    'Billie lançou sua primeira música "Ocean eyes" na SoundCloud, no ano de 2015 com 14 anos.'
+)
+
+criaCartao(
+    'Adolescência',
+    'Qual a síndrome de Billie?',
+    'Síndrome de Tourette.'
+)
+
+criaCartao(
+    'Auge da carreira',
+    'Quantas estatuetas Billie Eilish ganhou em sua primeira participação no Grammy em 2019?',
+    'A cantora norte americana ganhou 5 troféus em uma só noite.'
+)
+
+criaCartao(
+    'Vida pessoal',
+    'Qual é o nome do cachorrinho da artista?',
+    'Shark, cachorro resgatado por Billie Eilish tem 4 anos atualmente.'
+)
+
+criaCartao(
+    'Pais',
+    'Qual o nome dos pais da cantora?',
+    'A mãe de Billie se chama Maggie Baird, e seu pai se chama Patrick Oćonnell.'
+)
+criaCartao(
+    'Discografias',
+    'Quantos álbuns a cantora tem?',
+    'Atualmente a cantora possui 3 álbuns de estúdio.'
+)
+
+criaCartao(
+    'Canção mais famosa',
+    'Qual a música mais tocada da Billie?',
+    '"Bad guy" de Billie Eilish chega a incríveis 2 bilhões de streams no Spotify.'
+)
+
+criaCartao(
+    'Idade',
+    'Quantos anos Billie tem?',
+    'Atualmente a cantora californiana tem 22 anos.'
+)
